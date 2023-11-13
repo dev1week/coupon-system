f0f680f3d27ad4e7caebca97dd36427d90fd7865
dev1week <rondo2860@gmail.com>
2023-11-13 17:02:40 +0900

    feat: 유저당 1개의 쿠폰 발급 제한(#4)

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -15,14 +15,25 @@ public class ApplyService {
 
     private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+
+    private final AppliedUserRepository appliedUserRepository;
+
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId){
         //자바의
+        Long apply = appliedUserRepository.add(userId);
+
+        //지원 개수가 1보다 클 경우 이미 발급 받았던 이력이 있는 유저임
+        if(apply!=1){
+            return;
+        }
 
 
         //아래 로직 전체에 락을 걸게된다면 너무 느려진다.
--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -42,7 +42,7 @@ class ApplyServiceTest {
 
 
         for(int i=0; i<threadCount; i++){
-            long userId = 1;
+            long userId = i;
 
             executorService.submit(()->{
                 try {
@@ -55,9 +55,41 @@ class ApplyServiceTest {
 
         latch.await();
 
+        Thread.sleep(10000);
+
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(100);
+
+    }
+
+    @Test
+    public void 한명당_한개의쿠폰만_발급() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+
+        for(int i=0; i<threadCount; i++){
+            long userId = 1;
+
+            executorService.submit(()->{
+                try {
+                    applyService.apply(1L);
+                }finally{
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Thread.sleep(10000);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
 
     }
 
