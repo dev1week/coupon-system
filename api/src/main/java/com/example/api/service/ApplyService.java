181c56a657c7a251c0dd2ad9021aa39de9faffec
dev1week <rondo2860@gmail.com>
2023-11-13 16:02:39 +0900

    feat: redis로 레이쓰 컨디션 해결(#2)

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -9,13 +9,25 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId){
-        long count = couponRepository.count();
+        //자바의
+
+
+        //아래 로직 전체에 락을 걸게된다면 너무 느려진다.
+
+        //쿠폰 개수에 대한 정합성만 관리하면되므로 해당 부분만 락을 걸어준다.
+        //레디스는 싱글 스레드 기반으로 동작하므로 레이스 컨디션을 해결할 수 있다.
+
+//        long count = couponRepository.count();
+        Long count = couponCountRepository.increment();
 
         if(count>100){
             return;
